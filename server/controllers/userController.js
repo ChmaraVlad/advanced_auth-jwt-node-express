60b973857c781508542f15f5a4bf4aa24e0ce740
Vlad Chmara <chmaravlad5@gmail.com>
2023-10-23 21:29:10 +0300

    added user controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,54 @@
+const UserService = require("../service/userService")
+
+exports.registration = async (req, res, next) => {
+    try {
+        const {email, password} = req.body
+        const userData = await UserService.registration(email, password)
+
+        res.cookie('refreshToken', userData.refreshToken,
+        {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+
+        res.json({userData})
+    } catch (error) {
+        console.log('userController registration => error: ', error);
+    }
+}
+exports.login = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        console.log('userController login => error: ', error);
+    }
+}
+exports.logout = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        console.log('userController logout => error: ', error);
+    }
+}
+exports.activate = async (req, res, next) => {
+    try {
+        const activationLink = req.params.link
+        await UserService.activate(activationLink)
+
+        return res.redirect(process.env.CLIENT_URL)
+    } catch (error) {
+        console.log('userController activate => error: ', error);
+    }
+}
+exports.refresh = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        console.log('userController refresh => error: ', error);
+    }
+}
+exports.users = async (req, res, next) => {
+    try {
+        const users = await UserService.getAllUsers()
+        res.json({users})
+    } catch (error) {
+        console.log('userController users => error: ', error);
+    }
+}
