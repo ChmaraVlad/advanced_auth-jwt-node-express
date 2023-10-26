1b51aa77e4704d84e0f790c95396b55aa222f5a4
Vlad Chmara <chmaravlad5@gmail.com>
2023-10-26 17:32:08 +0300

    updated cors middleware server side

--- server/index.js
+++ server/index.js
@@ -1,47 +1,49 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const cookieParser = require('cookie-parser')
-const morgan = require('morgan')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 
 // routes
-const apiRouter = require('./router/index')
+const apiRouter = require("./router/index");
 
 // middlewares
-const errorMiddleware = require('./middlewares/errorMiddleware')
+const errorMiddleware = require("./middlewares/errorMiddleware");
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
 const dbConnection = async () => {
   await mongoose.connect(process.env.DB_URL);
-}
+};
 
-app.use(express.json())
+app.use(express.json());
 // для работы с куки
-app.use(cookieParser())
+app.use(cookieParser());
 // чтобы не было проблем с запросами в браузере
-app.use(cors())
+app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }));
 // middleware for logs
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
-app.use('/api', apiRouter)
+app.use("/api", apiRouter);
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
 const start = async () => {
-    try {
-        dbConnection()
-        .then(()=>console.log('DB connection was successfuly'))
-        .catch((error)=>{'DB connection was failed => error: ', error})
+  try {
+    dbConnection()
+      .then(() => console.log("DB connection was successfuly"))
+      .catch((error) => {
+        "DB connection was failed => error: ", error;
+      });
 
-        app.listen(PORT, ()=>console.log(`Server started on PORT = ${PORT}`))
-    } catch (error) {
-        console.log('ERROR ON START CONNECTING => ERROR: ', error);
-    }
-}
+    app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
+  } catch (error) {
+    console.log("ERROR ON START CONNECTING => ERROR: ", error);
+  }
+};
 
-start()
+start();
